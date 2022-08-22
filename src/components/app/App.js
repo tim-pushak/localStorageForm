51af688212099129c2d414496cde05bf05336333
Artem <pushakartem@gmail.com>
2022-08-22 17:41:15 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form1 from '../Form1/Form1';
+import Form2 from '../Form2/Form2';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>localStorage with React hooks</h1>
+        <Form1 />
+        <Form2 />
+      </div>
+    </div>
+  );
+}
+
+export default App;
